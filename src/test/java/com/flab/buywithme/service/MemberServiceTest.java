963b7d539e7f0b0e530451b8383344756c1a2a24
mgunho <mgunho18@gmail.com>
2022-12-17 03:36:31 +0900

    [#3] test : service layer test를 unit test로 변경

--- src/test/java/com/flab/buywithme/service/MemberServiceTest.java
+++ src/test/java/com/flab/buywithme/service/MemberServiceTest.java
@@ -2,41 +2,60 @@ package com.flab.buywithme.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 import com.flab.buywithme.domain.Address;
 import com.flab.buywithme.domain.Member;
+import com.flab.buywithme.repository.MemberRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
-@SpringBootTest
-@Transactional
-class MemberServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceTest {
 
-    @Autowired
-    MemberService memberService;
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
 
     @Test
     public void 회원가입() {
         //given
-        Member member = new Member(new Address("성남시", "분당구", "판교동"), "kim", "010-1111-1111", "test",
-                "test");
+        Member member = fakeMember();
+
+        Long fakeMemberId = 1L;
+        ReflectionTestUtils.setField(member, "id", fakeMemberId);
+
+        //mocking
+        given(memberRepository.save(any()))
+                .willReturn(member);
+        given(memberRepository.findById(fakeMemberId))
+                .willReturn(Optional.ofNullable(member));
 
         //when
         Long saveId = memberService.join(member);
 
         //then
-        assertEquals(member, memberService.findOne(saveId));
+        assertEquals(member, memberService.findById(saveId).orElse(null));
     }
 
     @Test
     public void 중복회원예외() {
         //given
-        Member member1 = new Member(new Address("성남시", "분당구", "판교동"), "kim", "010-1111-1111",
-                "test", "test1");
-        Member member2 = new Member(new Address("성남시", "분당구", "판교동"), "lee", "010-2222-2222",
-                "test", "test2");
+        Member member1 = fakeMember();
+        Member member2 = fakeMember();
+
+        //mocking
+        given(memberRepository.save(any()))
+                .willReturn(member1)
+                .willThrow(IllegalStateException.class);
 
         //when, then
         assertThrows(IllegalStateException.class, () -> {
@@ -44,4 +63,10 @@ class MemberServiceTest {
             memberService.join(member2);
         });
     }
+
+    private Member fakeMember() {
+        return new Member(new Address("성남시", "분당구", "판교동"), "kim", "010-1111-1111",
+                "test", "test1");
+    }
+
 }
