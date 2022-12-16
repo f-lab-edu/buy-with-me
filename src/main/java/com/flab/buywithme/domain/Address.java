2ef50e209cd2f78fd4fcdcceb02e562225e86ce4
mgunho <mgunho18@gmail.com>
2022-12-17 03:23:47 +0900

    [#3] feature : 1) address table에 unique 제약 조건 추가, 2) address id 값을 기준으로 equals, hashcode method 정의

--- src/main/java/com/flab/buywithme/domain/Address.java
+++ src/main/java/com/flab/buywithme/domain/Address.java
@@ -5,10 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UniqueAddress", columnNames = {"depth1", "depth2", "depth3"})})
+@EqualsAndHashCode(of = "id")
 @Getter
 @NoArgsConstructor
 public class Address {
