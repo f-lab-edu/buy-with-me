306d943a8d103c9eef58412c51bd28b6ce774607
mgunho <mgunho18@gmail.com>
2022-12-17 03:27:56 +0900

    [#3] feature : JpaRepository 제공 method 사용하도록 변경

--- src/main/java/com/flab/buywithme/repository/MemberRepository.java
+++ src/main/java/com/flab/buywithme/repository/MemberRepository.java
@@ -1,29 +1,12 @@
 package com.flab.buywithme.repository;
 
 import com.flab.buywithme.domain.Member;
-import java.util.List;
-import javax.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findByLoginId(String loginId) {
-        return em.createQuery("select u from Member u where u.loginId = :loginId", Member.class)
-                .setParameter("loginId", loginId)
-                .getResultList();
-    }
-
+    Optional<Member> findByLoginId(String loginId);
 }
--- src/main/java/com/flab/buywithme/service/MemberService.java
+++ src/main/java/com/flab/buywithme/service/MemberService.java
@@ -2,7 +2,7 @@ package com.flab.buywithme.service;
 
 import com.flab.buywithme.domain.Member;
 import com.flab.buywithme.repository.MemberRepository;
-import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,14 +22,14 @@ public class MemberService {
     }
 
     private void checkDuplicateMemberExists(Member member) {
-        List<Member> findMember = memberRepository.findByLoginId(member.getLoginId());
-        if (!findMember.isEmpty()) {
+        Optional<Member> findMember = memberRepository.findByLoginId(member.getLoginId());
+        if (findMember.isPresent()) {
             throw new IllegalStateException("이미 존재하는 회원입니다!");
         }
     }
 
-    public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+    public Optional<Member> findById(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 
 }
