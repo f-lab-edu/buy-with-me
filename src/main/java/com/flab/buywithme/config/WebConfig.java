81d5cb013e7ea744d252f6ae3f69b6adcfbe486f
mgunho <mgunho18@gmail.com>
2022-12-30 21:20:29 +0900

    [#4] feature: 로그인 check하는 인터셉터 추가

---
+++ src/main/java/com/flab/buywithme/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.flab.buywithme.config;
+
+import com.flab.buywithme.interceptor.LoginCheckInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/members", "/members/signin");
+    }
+}
