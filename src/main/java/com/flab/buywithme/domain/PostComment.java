78cc495505654b746d1f59a4ed1589e4741b78a3
mgunho <mgunho18@gmail.com>
2023-01-13 05:24:02 +0900

    [#11] feature : PostComment domain, DTO 작성

---
+++ src/main/java/com/flab/buywithme/domain/PostComment.java
@@ -0,0 +1,58 @@
+package com.flab.buywithme.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = "id")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public boolean checkIsOwner(Long memberId) {
+        return this.member.getId().equals(memberId);
+    }
+}
