41291f067ac1549e4507db0f9c10b2f81884609b
mgunho <mgunho18@gmail.com>
2023-01-22 01:45:01 +0900

    [#12] feature : EnrollController 작성

---
+++ src/main/java/com/flab/buywithme/controller/EnrollController.java
@@ -0,0 +1,30 @@
+package com.flab.buywithme.controller;
+
+import com.flab.buywithme.service.EnrollService;
+import com.flab.buywithme.utils.SessionConst;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@RestController
+@RequestMapping("/posts/{postId}")
+@RequiredArgsConstructor
+public class EnrollController {
+
+    private final EnrollService enrollService;
+
+    @PostMapping("/join")
+    public void join(@PathVariable Long postId,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId) {
+        enrollService.joinBuying(postId, memberId);
+    }
+
+    @PostMapping("/cancel")
+    public void cancel(@PathVariable Long postId,
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER) Long memberId) {
+        enrollService.cancelJoining(postId, memberId);
+    }
+}
