cbf40c2845a900571c40ee8f2b2dc1d4ecf7d58c
GunHo Moon <33826145+mgunho18@users.noreply.github.com>
2023-03-21 03:13:23 +0900

    [#44] refactor: LazyConnectionDataSourceProxy 빈 등록

---
+++ src/main/java/com/flab/buywithme/config/DataSourceConfig.java
@@ -0,0 +1,22 @@
+package com.flab.buywithme.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource lazyDataSource(DataSourceProperties properties) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        return new LazyConnectionDataSourceProxy(dataSource);
+    }
+}
